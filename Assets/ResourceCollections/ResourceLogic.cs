9d042573c7b15f2c57ee55d75a08d503132fe301
TheCell <simon_hischier@hotmail.com>
2020-02-01 22:30:14 +0100

    added resource images

--- Assets/ResourceCollections/ResourceLogic.cs
+++ Assets/ResourceCollections/ResourceLogic.cs
@@ -6,6 +6,7 @@ public class ResourceLogic : MonoBehaviour
 {
     public ResourceType typeOfResource;
     public DrillResources drillResources;
+    [SerializeField] private Sprite[] spritesForTypes;
     private SpriteRenderer spriteRenderer;
 
     public void SetResourceType(ResourceType type)
@@ -60,19 +61,24 @@ public class ResourceLogic : MonoBehaviour
         switch(typeOfResource)
         {
             case ResourceType.Zucker:
-                spriteRenderer.color = new Color(1f, 1f, 1f);
+                // spriteRenderer.color = new Color(1f, 1f, 1f);
+                spriteRenderer.sprite = spritesForTypes[0];
                 break;
             case ResourceType.Salat:
-                spriteRenderer.color = new Color(0.3f, .88f, 0.3f);
+                // spriteRenderer.color = new Color(0.3f, .88f, 0.3f);
+                spriteRenderer.sprite = spritesForTypes[1];
                 break;
             case ResourceType.Kaugummi:
-                spriteRenderer.color = new Color(0.89f, 0.37f, 0.77f);
+                // spriteRenderer.color = new Color(0.89f, 0.37f, 0.77f);
+                spriteRenderer.sprite = spritesForTypes[2];
                 break;
             case ResourceType.Kaese:
-                spriteRenderer.color = new Color(0.87f, 0.87f, 0.12f);
+                // spriteRenderer.color = new Color(0.87f, 0.87f, 0.12f);
+                spriteRenderer.sprite = spritesForTypes[3];
                 break;
             case ResourceType.Caramel:
-                spriteRenderer.color = new Color(0.9716981f, 0.6696121f, 0.3529281f);
+                // spriteRenderer.color = new Color(0.9716981f, 0.6696121f, 0.3529281f);
+                spriteRenderer.sprite = spritesForTypes[4];
                 break;
             default:
                 break;
