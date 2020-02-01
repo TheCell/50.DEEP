92032d1140778ca7b357ad000a77f1c2114ebdc6
Fl0ps <florian.etgeton@gmail.com>
2020-02-01 20:52:28 +0100

    buttons placed

--- Assets/ButttonController.cs
+++ Assets/ButttonController.cs
@@ -6,14 +6,30 @@ using UnityEngine.InputSystem;
 
 public class ButttonController : MonoBehaviour
 {
+    public Sprite buttonPressed;
     public UnityEvent ButtonPressed;
+
+    private Sprite ButtonUp;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+        ButtonUp = sr.sprite;
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            sr.sprite = buttonPressed;
             ButtonPressed.Invoke();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        sr.sprite = ButtonUp;
     }
 
     // Just some debugging stuff
