a9b5c9e30732e2de2b08bb1e7a1a3fe96cc7847e
Fl0ps <florian.etgeton@gmail.com>
2020-02-01 18:01:34 +0100

    Panzer is moving

---
+++ Assets/ButttonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class ButttonController : MonoBehaviour
+{
+    public UnityEvent ButtonPressed;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ButtonPressed.Invoke();
+        }
+    }
+
+    // Just some debugging stuff
+    //private void Update()
+    //{
+    //    if (Keyboard.current.gKey.isPressed)
+    //    {
+    //        ButtonPressed.Invoke();
+    //    }
+    //}
+}
