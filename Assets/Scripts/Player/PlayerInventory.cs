121f750f1fbd2a29fa044b247a7eaa2c3373f5c0
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2020-02-02 14:43:23 +0100

    player inventory

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -6,6 +6,7 @@ public class PlayerInventory: MonoBehaviour
 {
 
     public DrillResources dr;
+    public int maxAmount = 100;
 
     private void Start()
     {
@@ -14,23 +15,8 @@ public class PlayerInventory: MonoBehaviour
 
     public void AddItem(ResourceType type)
     {
-        
-        switch (type)
-        {
-            case ResourceType.Antrieb:
-                dr.Antrieb += 1;
-                break;
-            case ResourceType.Baumaterial:
-                dr.Baumaterial += 1;
-                break;
-            case ResourceType.Wasser:
-                dr.Wasser += 1;
-                break;
-        }
 
-        DrillController.dr.Antrieb = -dr.Antrieb;
-        DrillController.dr.Baumaterial = -dr.Baumaterial;
-        DrillController.dr.Wasser = -dr.Wasser;
+        Withdraw(dr, type);
 
         Debug.Log(dr.Antrieb + " " + dr.Baumaterial + " " + dr.Wasser);
         Debug.Log(DrillController.dr.Antrieb + " " + DrillController.dr.Baumaterial + " " + DrillController.dr.Wasser);
@@ -42,5 +28,61 @@ public class PlayerInventory: MonoBehaviour
         dr = other.GetComponent<ObjectRepairLogic>().Repair(dr);
 
         Debug.Log(dr.Antrieb + " " + dr.Baumaterial + " " + dr.Wasser);
+    }
+
+
+    private DrillResources Withdraw (DrillResources playerResources, ResourceType type)
+    {
+        DrillResources controller = DrillController.dr;
+
+        switch (type)
+        {
+            case ResourceType.Antrieb:
+                int amountAntriebMissing = maxAmount - playerResources.Antrieb;
+
+                if (controller.Antrieb >= amountAntriebMissing)
+                {
+                    controller.Antrieb -= amountAntriebMissing;
+                    playerResources.Antrieb = maxAmount;
+                } else
+                {
+                    playerResources.Antrieb += controller.Antrieb;
+                    controller.Antrieb = 0;
+
+                }          
+                break;
+            case ResourceType.Baumaterial:
+                int amountBaumaterialMissing = maxAmount - playerResources.Baumaterial;
+
+                if (controller.Baumaterial >= amountBaumaterialMissing)
+                {
+                    controller.Baumaterial -= amountBaumaterialMissing;
+                    playerResources.Baumaterial = maxAmount;
+                }
+                else
+                {
+                    playerResources.Baumaterial += controller.Baumaterial;
+                    controller.Baumaterial = 0;
+
+                }
+                break;
+            case ResourceType.Wasser:
+                int amountWasserMissing = maxAmount - playerResources.Wasser;
+
+                if (controller.Wasser >= amountWasserMissing)
+                {
+                    controller.Wasser -= amountWasserMissing;
+                    playerResources.Wasser = maxAmount;
+                }
+                else
+                {
+                    playerResources.Wasser += controller.Wasser;
+                    controller.Wasser = 0;
+
+                }
+                break;
+        }
+
+        return playerResources;
     }
 }
