9e115ae801deb3ec6f3fa3db8f15e0d4beb8fd7c
Fl0ps <florian.etgeton@gmail.com>
2020-02-02 01:04:32 +0100

    Implement El-Vator

---
+++ Assets/ElevatorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    [SerializeField] private GameObject firstPosition;
+    [SerializeField] private GameObject secondPosition;
+    [SerializeField] private float speed;
+
+    private GameObject targetPostion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPostion = firstPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector2.MoveTowards(this.transform.position, targetPostion.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        targetPostion = secondPosition;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targetPostion = firstPosition;
+    }
+}
